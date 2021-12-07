09e7739977d908db98afe46c75cc783d9c1b9eeb
syedamariumhasnain <syedamarium27@gmail.com>
2021-12-07 12:58:57 +0500

    moves logics to controllers folder + leave routes for path mapping

--- app.js
+++ app.js
@@ -2,17 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
-// const usersRouters = require("./routes/users-routes");
 
 const app = express();
 
 app.use("/api/places", placesRoutes); //  => /api/places/...
-// app.use("/api/users", usersRouters);
 
-// Special Middleware function with 4 args is considered as error handling 
-// middleware function by express, having first arg "error"
-// This function will only be executed on the requests that have an error 
-// attached to it, when any middleware function infront of it yeilds an error
 app.use((error, req, res, next) => {
   if(res.headerSent) {
     return next(error);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,55 +1,13 @@
+// Routes for MAPPING / PATHS
+
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-// if we had somthing specific like "/user" then we will put it
-// above this "/:pid" route, to handle that specific case and
-// not considered as some "pid" has a value "user"
-
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
-
-  if (!place) {
-    // 2 Things we can do, either throw error or pass it using to next
-    // In asynchronous code, we need to use next to pass error through and, 
-    // In synchronous code, we can just simply throw the error
-    throw new HttpError("Could not find a place for the provided id.", 404);
-    // return next(new HttpError("Could not find a place for the provided id.", 404));
-  }
-
-  res.json({ place }); // => { place } => { place: place }
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((u) => u.creator === userId);
-
-  if (!place) {
-    // throw new HttpError("Could not find a place for the provided user id.", 404);
-    return next(new HttpError("Could not find a place for the provided user id.", 404));
-    // Note: here if we don't return at this statement the next statement 
-    // res.json({...}) will also be executed, causing two diff. responses, 
-    // hence generating error at the backend.
-  }
-
-  res.json({ place });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
