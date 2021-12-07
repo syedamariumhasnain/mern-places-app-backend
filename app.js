b8464526d076df4c232ec6a9e4303501ab0c6f6b
syedamariumhasnain <syedamarium27@gmail.com>
2021-12-07 17:29:49 +0500

    added POST request createPlace with unique id by uuid package + test requests on postman

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
+// This middleware extracts the json data from any incomming request's body, 
+// convert in regular JS data structure (object, array, ...) and automatically 
+// call next(), to reach next middleware inline 
+app.use(bodyParser.json());
+
 app.use("/api/places", placesRoutes); //  => /api/places/...
 
 app.use((error, req, res, next) => {
--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,5 +1,6 @@
 // Controllers for LOGIC / MIDDLEWARE FUNCTIONS
 
+const uuid = require("uuid");
 const HttpError = require("../models/http-error");
 
 const DUMMY_PLACES = [
@@ -38,5 +39,26 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 }
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body; 
+  // Extracting data by object destructuring. Similar to:
+  // const title = req.body.title;
+  const createdPlace = {
+    id: uuid.v4(),
+    title,  // => title => (similar to) => title: title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
+
+  DUMMY_PLACES.push(createdPlace);  // unshift(createPlace) -- to put at first index 
+  
+  // status 200 -- normal success
+  // status 201 -- something created successfully 
+  res.status(201).json({place: createdPlace});
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
