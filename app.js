5babdf9671def02b0278bf1920abb643457b6d2d
syedamariumhasnain <syedamarium27@gmail.com>
2021-12-07 11:55:50 +0500

    added error handling middleware function

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ const app = express();
 app.use("/api/places", placesRoutes); //  => /api/places/...
 // app.use("/api/users", usersRouters);
 
+// Special Middleware function with 4 args is considered as error handling 
+// middleware function by express, having first arg "error"
+// This function will only be executed on the requests that have an error 
+// attached to it, when any middleware function infront of it yeilds an error
+app.use((error, req, res, next) => {
+  if(res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({message: error.message || "An unknown error occurred!"})
+});
+
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -9,27 +9,49 @@ const DUMMY_PLACES = [
     description: "One of the most famous sky scrapers in the world",
     location: {
       lat: 40.7484474,
-      lng: -73.9871516
+      lng: -73.9871516,
     },
     address: "20 W 34th St, New York, NY 10001",
     creator: "u1",
-  }
+  },
 ];
 
-// if we had somthing specific like "/user" then we will put it 
-// above this "/:pid" route, to handle that specific case and 
+// if we had somthing specific like "/user" then we will put it
+// above this "/:pid" route, to handle that specific case and
 // not considered as some "pid" has a value "user"
 
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find(p => p.id === placeId);
-  res.json({place}); // => { place } => { place: place }
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    // 2 Things we can do, either throw error or pass it using to next
+    // In asynchronous code, we need to use next to pass error through and, 
+    // In synchronous code, we can just simply throw the error
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    throw error;
+    // next(error);
+  }
+
+  res.json({ place }); // => { place } => { place: place }
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find(u => u.creator === userId);
-  res.json({place});
-})
+  const place = DUMMY_PLACES.find((u) => u.creator === userId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided user id.");
+    error.code = 404;
+    // throw error;
+    return next(error);
+    // Note: here if we don't return at this statement the next statement 
+    // res.json({...}) will also be executed, causing two diff. responses, 
+    // hence generating error at the backend.
+  }
+
+  res.json({ place });
+});
 
 module.exports = router;
