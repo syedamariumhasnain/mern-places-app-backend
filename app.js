a8de9ada4b19b3f380501082c4686524850ec07b
syedamariumhasnain <syedamarium27@gmail.com>
2021-12-08 17:56:05 +0500

    getUsers + signup + login -- controllers and routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
@@ -12,12 +13,14 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes); //  => /api/places/...
+app.use("/api/users", usersRoutes); 
 
 // Error Handling for unsupported routes
 app.use((req, res, next) => {
   throw new HttpError("Could not find this route.", 404);
 });
 
+// Default Error Handler
 app.use((error, req, res, next) => {
   if(res.headerSent) {
     return next(error);
