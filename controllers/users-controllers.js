2a0d9ec92f343c26cc93f0675a7b1f75152f806d
syedamariumhasnain <syedamarium27@gmail.com>
2021-12-10 16:23:34 +0500

    user login (check of credentials)

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -52,15 +52,20 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    // HTTP code 401 - Authentication Fails
-    throw new HttpError(
-      "Could not identify user, credentials seem to be wrong", 401
-    );
+  let existingUser; 
+  try {
+    existingUser = await User.findOne({ email: email });
+    if(!existingUser || existingUser.password !== password) {
+      // HTTP code 401 - Authentication Fails
+      const error = new HttpError("Could not identify user, credentials seem to be wrong", 401);
+      return next(error);
+    }
+  } catch(err) {
+    const error = new HttpError("Login failed, please try again later.", 500);
+    return next(error);
   }
 
   res.json({ message: "Logged in!" });
