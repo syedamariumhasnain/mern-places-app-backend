30d29a2ef867a41c9c73791e037c74caa057cdc2
syedamariumhasnain <syedamarium27@gmail.com>
2021-12-10 16:39:19 +0500

    get all users

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -13,8 +13,17 @@ const DUMMY_USERS = [
   },
 ];
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    // users = User.find({}, "email name");  // Returns email and name of all users    
+    users = await User.find({}, "-password");  // Returns all data fields except password of all users 
+  } catch(err) {
+    const error = new HttpError("Fetching users failed, please try again later.", 500);
+    return next(error);
+  }
+
+  res.json({ users: users.map(user => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
