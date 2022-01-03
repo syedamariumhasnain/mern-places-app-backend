335c4bd70ec6624230c3bd10ccd8589880cec067
syedamariumhasnain <syedamarium27@gmail.com>
2022-01-03 17:15:50 +0500

    added PORT env

--- app.js
+++ app.js
@@ -57,5 +57,5 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q2val.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => console.log(err));
